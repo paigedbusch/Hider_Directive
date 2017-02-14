bde3025aa94f0548559288854a7f60cd4622eb18
Paige Busch <93paigedf@gmail.com>
2017-02-14 15:15:24 -0700

    final commit

---
+++ lessonCtrl.js
@@ -0,0 +1,7 @@
+angular.module('directivePractice').controller('lessonCtrl', function($scope, lessonService) {
+    $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+    $scope.getSchedule = lessonService.getSchedule();
+    $scope.announceDay = function(lesson, day) {
+        alert(lesson + ' is active on ' + day + '.');
+    }
+});
